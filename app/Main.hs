0a95d8618c4b77ab0d5435bad03d0766912ea9fe
Jappie Klooster <jappieklooster@hotmail.com>
2023-02-26 22:55:42 +0100

    try some effect stuff

--- app/Main.hs
+++ app/Main.hs
@@ -6,22 +6,25 @@ import Network.HTTP.Types.Status
 import Data.Binary.Builder
 import Data.Text.Encoding
 import Data.Text
+import Control.Monad.Except
 
 main :: IO ()
 main =
   Server.host Server.defaultSettings $ do
     requestEvt <- Server.getRequest
-    Server.writeResponse $ (\(token, request) -> (token,
-                                                  case toLazyByteString . encodeUtf8Builder <$> responseMap request of
-                                                    Nothing -> (responseLBS notFound404 [] "not found")
-                                                    Just resp -> responseLBS ok200 [] resp
-                                                 )
+    Server.writeResponse $ (\(token, request) -> do
+                                                 res  <- runExceptT $ responseMap request
+                                                 case res of
+                                                    Left NotFound -> pure (token ,responseLBS notFound404 [] "not found")
+                                                    Right resp -> pure (token, responseLBS ok200 [] $ toLazyByteString $ encodeUtf8Builder resp)
                            ) <$> requestEvt
 
-responseMap :: Request -> Maybe Text
-responseMap req = case rawPathInfo req of
-  "/jappie" -> Just "hello jappie!"
-  "/luci" -> Just "hi lucy ❤️"
-  "/duck" -> Just "duck duck duck."
-  "/eru" -> Just "ewwwuuu~"
-  _ -> Nothing
+data ResponseErrors = NotFound
+
+responseMap :: MonadError ResponseErrors m => Request -> m Text
+responseMap req = case pathInfo req of
+  ["jappie"] -> pure "hello jappie!"
+  ["luci"] -> pure "hi lucy ❤️"
+  ["duck"] -> pure "duck duck duck."
+  ["eru"] -> pure "ewwwuuu~"
+  _ -> throwError NotFound
